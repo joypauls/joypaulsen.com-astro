c14d9841b5104f949ac8f77f90107c46a374de3c
joypauls <joypaulsen3@gmail.com>
2025-06-04 18:39:10 -0500

    components for link buttons

---
+++ src/links.ts
@@ -0,0 +1,13 @@
+import Github from "./icons/github.svg";
+import LinkedIn from "./icons/linkedin.svg";
+import Email from "./icons/email.svg";
+import Feed from "./icons/feed.svg";
+import Person from "./icons/person.svg";
+
+export const links = [
+  { label: "About Me", href: "/about", icon: Person  },
+  { label: "Blog", href: "https://joypauls.dev", icon: Feed  },
+  { label: "Github", href: "https://github.com/joypauls", icon: Github },
+  { label: "LinkedIn", href: "https://www.linkedin.com/in/joy-paulsen-0bb55419b/", icon: LinkedIn },
+  { label: "Email", href: "mailto:hello@joypauls.dev", icon: Email },
+];
