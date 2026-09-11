fc836ff84818dc5c92ebf4edaeb385cf20f91026
joypauls <joypaulsen3@gmail.com>
2026-09-10 20:36:40 -0500

    add back in blog link

--- src/links.ts
+++ src/links.ts
@@ -5,8 +5,8 @@ import Feed from "./icons/feed.svg";
 import Person from "./icons/person.svg";
 
 export const links = [
-  { label: "About Me", href: "/about", icon: Person  },
-  // { label: "Blog", href: "https://joypauls.dev", icon: Feed  },
+  { label: "About Me", href: "/about", icon: Person },
+  { label: "Blog", href: "https://joypauls.dev", icon: Feed },
   { label: "Github", href: "https://github.com/joypauls", icon: Github },
   { label: "LinkedIn", href: "https://www.linkedin.com/in/joy-paulsen-0bb55419b/", icon: LinkedIn },
   { label: "Email", href: "mailto:hello@joypauls.dev", icon: Email },
